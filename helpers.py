4d5b2519c9858ce9b9c6800eea5bf93407ee6795
Julius Wantenaar <j.wantenaar@hotmail.com>
2021-11-30 15:38:45 +0100

    cleaned up code. Added globals to easily compare simulations.

--- helpers.py
+++ helpers.py
@@ -1,14 +1,26 @@
 import simpy
 import random
 import numpy as np
-import scipy.stats as st
-import matplotlib.pyplot as plt
 from joblib import Parallel, delayed
 
 
 class queuSim():
     def __init__(self, randomSeed = 42, newCustomers = 500, intervalCustomers = 2, serviceTime = 2, numSim = 100, 
     capacity = 1, arrivalDistribution = "M", serviceDistribution = "M", helpStrat = "FIFO", verbose=False):
+        """creates a class that runs a simulation of a queue.
+
+        Args:
+            randomSeed (int, optional): Choose a seed to have similarity between simulations. Defaults to 42.
+            newCustomers (int, optional): The amount of customers that visits per simulation. Defaults to 500.
+            intervalCustomers (int, optional): The average interval between customers. Defaults to 2.
+            serviceTime (int, optional): The average time a customer occupies a server. Defaults to 2.
+            numSim (int, optional): The amount of simulations ran. Defaults to 100.
+            capacity (int, optional): The amount of servers in the system. Defaults to 1.
+            arrivalDistribution (str, optional): The distribution from which the arrival intervals are drawn. Defaults to "M".
+            serviceDistribution (str, optional): The distribution from which the service times are drawn. Defaults to "M".
+            helpStrat (str, optional): The strategy of servers to choose which customer to help. Defaults to "FIFO".
+            verbose (bool, optional): Let the program print statements for every event. WARNING: this heavily decreases performance. Defaults to False.
+        """
         assert helpStrat == "SJF" or helpStrat == "FIFO", "This helpstrat has not yet been implemented"
         assert serviceDistribution == "M" or serviceDistribution == "D" or serviceDistribution  == "H", "This service distribution has not yet been implemented"
 
@@ -27,11 +39,24 @@ class queuSim():
 
 
     def runSim(self):
+        """main loop of the simulation, change the number of workers to fit your system.
+
+        Returns:
+            list: Returns a list of all waiting times of customers.
+        """
         waitTimes = Parallel(n_jobs=8)(delayed(self.process)(i) for i in range(self.numSim))
-        return waitTimes
+        return waitTimes[0]
 
 
     def process(self,i):
+        """Caller function that calls initiates the simulation.
+
+        Args:
+            i (int): This is only used for parallelization, should not be used for
+
+        Returns:
+            list: Returns a list of waiting times of customers.
+        """
         waitTimes = []
         env = simpy.Environment()
         if self.helpStrat == "SJF":
@@ -46,16 +71,37 @@ class queuSim():
 
 
     def source(self, env, servers, waitTimes, verbose):
-        """Source generates customers randomly"""
+        """Creates servers within the simpy environment.
+
+        Args:
+            env (Simpy environment): This is the environment in which the simulation is running.
+            servers (Simpy resource): The avalaible servers for this simulation.
+            waitTimes (list): A python list object in which the waiting times are saved.
+            verbose (Bool): Choose whether or not to print status updates.
+
+        Yields:
+            environment timeout: A simpy function to progress the time when a customer arrives.
+        """
         for i in range(self.newCustomers):
-            c = self.customer(env, 'Customer%02d' % i, servers, self.serviceTime, waitTimes, verbose)
+            c = self.customer(env, 'Customer%02d' % i, servers, waitTimes, verbose)
             env.process(c)
             t = random.expovariate(1.0 / self.intervalCustomers) # markovian arrival rate
             yield env.timeout(t)
     
     
-    def customer(self, env, name, servers, serviceTime, waitTimes, verbose):
-        """Customer arrives, is served and leaves."""
+    def customer(self, env, name, servers, waitTimes, verbose):
+        """A function that uses simpy logic to make a customer look for a avalaible desk and make the time progress in the user specified way. 
+
+        Args:
+            env (Simpy environment): This is the environment in which the simulation is running.            
+            name (String): A name for the current customer. Only used when verbose is on.   
+            servers (Simpy resource): Simpy resources that simulate the servers.
+            waitTimes (list): A python list object to save the waiting times.
+            verbose (Bool): Choose whether or not to print status updates. 
+
+        Yields:
+            environment timeout: A simpy function to progress the time while the customer is being helped.
+        """
         arrive = env.now
         if verbose:
             print('%7.4f %s: Here I am' % (arrive, name))
@@ -69,7 +115,7 @@ class queuSim():
                 tib = random.expovariate(1/1)
         else:
             # markovian service rate
-            tib = random.expovariate(1.0 / serviceTime) 
+            tib = random.expovariate(1.0 / self.serviceTime) 
 
         if self.helpStrat == "SJF":
             with servers.request(priority = int(1/tib*1000)) as req:
